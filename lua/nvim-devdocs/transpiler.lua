4aa2c3aae22dce3c592f826d459047f25a2b0106
Luckas <luckasranarison@gmail.com>
2023-08-08 14:12:27 +0300

    wip: table transpilation

--- lua/nvim-devdocs/transpiler.lua
+++ lua/nvim-devdocs/transpiler.lua
@@ -10,6 +10,41 @@ local normalize_html = function(str)
   return str
 end
 
+local tag_mappings = {
+  h1 = { left = "# ", right = "\n\n" },
+  h2 = { left = "## ", right = "\n\n" },
+  h3 = { left = "### ", right = "\n\n" },
+  h4 = { left = "#### ", right = "\n\n" },
+  h5 = { left = "##### ", right = "\n\n" },
+  h6 = { left = "###### ", right = "\n\n" },
+  div = { right = "\n" },
+  section = { right = "\n" },
+  p = { right = "\n\n" },
+  ul = { right = "\n" },
+  ol = { right = "\n" },
+  dl = { right = "\n" },
+  dt = { right = "\n" },
+  figure = { right = "\n" },
+  dd = { left = ": " },
+  span = {},
+  header = {},
+  thead = {},
+  tbody = {},
+  pre = { left = "```\n", right = "```" },
+  code = { left = "`", right = "`" },
+  samp = { left = "`", right = "`" },
+  var = { left = "`", right = "`" },
+  kbd = { left = "`", right = "`" },
+  strong = { left = "**", right = "**" },
+  em = { left = " _", right = "_ " },
+  small = { left = " _", right = "_ " },
+  sup = { left = "^", right = "^" },
+  blockquote = { left = "> " },
+
+  br = { right = "\n" },
+  hr = { right = "---" },
+}
+
 M.to_yaml = function(entry)
   local lines = {}
 
@@ -27,39 +62,6 @@ M.to_yaml = function(entry)
   return table.concat(lines, "\n")
 end
 
-local tag_mappings = {
-  h1 = { left = "# ", right = "\n" },
-  h2 = { left = "## ", right = "\n" },
-  h3 = { left = "### ", right = "\n" },
-  h4 = { left = "#### ", right = "\n" },
-  h5 = { left = "##### ", right = "\n" },
-  h6 = { left = "###### ", right = "\n" },
-  div = { right = "\n" },
-  section = { right = "\n" },
-  p = { right = "\n" },
-  ul = { right = "\n" },
-  ol = { right = "\n" },
-  dl = { right = "\n" },
-  dt = { right = "\n" },
-  figure = { right = "\n" },
-  dd = { left = ": " },
-  span = {},
-  header = {},
-  pre = { left = "```", right = "```" },
-  code = { left = "`", right = "`" },
-  samp = { left = "`", right = "`" },
-  var = { left = "`", right = "`" },
-  kbd = { left = "`", right = "`" },
-  strong = { left = "**", right = "**" },
-  em = { left = " _", right = "_ " },
-  small = { left = " _", right = "_ " },
-  sup = { left = "^", right = "^" },
-  blockquote = { left = "> " },
-
-  br = { right = "\n" },
-  hr = { right = "---" },
-}
-
 M.html_to_md = function(html)
   local transpiler = {
     parser = vim.treesitter.get_string_parser(html, "html"),
@@ -68,7 +70,7 @@ M.html_to_md = function(html)
   }
 
   ---@param node TSNode
-  function transpiler:extract_node_text(node)
+  function transpiler:get_node_text(node)
     local row_start, col_start = node:start()
     local row_end, col_end = node:end_()
     local extracted_lines = {}
@@ -111,7 +113,7 @@ M.html_to_md = function(html)
   function transpiler:eval(node)
     local result = ""
     local node_type = node:type()
-    local node_text = self:extract_node_text(node)
+    local node_text = self:get_node_text(node)
 
     if node_type == "text" or node_type == "entity" then
       result = result .. normalize_html(node_text)
@@ -120,18 +122,21 @@ M.html_to_md = function(html)
       local tag_node = children[1]
       local tag_children = tag_node:named_children()
       local tag_type = tag_node:type()
-      local tag_name = self:extract_node_text(tag_node:named_child())
+      local tag_name = self:get_node_text(tag_node:named_child())
       local attributes = {}
 
       for i = 2, #tag_children do
         local attribute_node = tag_children[i]
         local attribute_name_node = attribute_node:named_child()
-        local attribute_name = self:extract_node_text(attribute_name_node)
+        local attribute_name = self:get_node_text(attribute_name_node)
         local value = ""
+
         if attribute_name_node:next_named_sibling() then
-          local value_node = attribute_name_node:next_named_sibling():named_child()
-          value = self:extract_node_text(value_node)
+          local quotetd_value_node = attribute_name_node:next_named_sibling()
+          local value_node = quotetd_value_node:named_child()
+          if value_node then value = self:get_node_text(value_node) end
         end
+
         attributes[attribute_name] = value
       end
 
@@ -141,36 +146,33 @@ M.html_to_md = function(html)
         end
       end
 
+      local skipable = { "tr", "td", "th" }
+      if vim.tbl_contains(skipable, tag_name) then return "" end
+
       if tag_name == "a" then
         result = string.format("[%s](%s)", result, attributes.href)
       elseif tag_name == "img" then
         result = string.format("![%s](%s)", attributes.alt, attributes.src)
       elseif tag_name == "pre" and attributes["data-language"] then
         result = "```" .. attributes["data-language"] .. "\n" .. result .. "\n```\n"
+      elseif tag_name == "table" then
+        result = self:eval_table(node)
+      elseif tag_name == "abbr" then
+        result = string.format("%s(%s)", result, attributes.title)
+      elseif tag_name == "iframe" then
+        result = string.format("[%s](%s)", attributes.title, attributes.src)
       elseif tag_name == "li" then
         local parent_node = node:parent()
         local parent_tag_name_node = parent_node:named_child():named_child()
-        local parent_tag_name = self:extract_node_text(parent_tag_name_node)
+        local parent_tag_name = self:get_node_text(parent_tag_name_node)
 
         if parent_tag_name == "ul" then result = "- " .. result .. "\n" end
         if parent_tag_name == "ol" then
           local siblings = parent_node:named_children()
           for i = 2, #siblings - 1 do
-            if node:equal(siblings[i]) then result = i - 1 .. " - " .. result .. "\n" end
+            if node:equal(siblings[i]) then result = i - 1 .. ". " .. result .. "\n" end
           end
         end
-      elseif tag_name == "table" or tag_name == "tbody" or tag_name == "thead" then
-        local table_children = node:named_children()
-        for i = 2, #table_children - 1 do
-          local child_node = table_children[i]
-          result = self:eval_row(child_node) .. "|"
-        end
-
-        result = "\n" .. node_text .. "\n"
-      elseif tag_name == "abbr" then
-        result = string.format("%s(%s)", result, attributes.title)
-      elseif tag_name == "iframe" then
-        result = string.format("[%s](%s)", attributes.title, attributes.src)
       else
         local map = tag_mappings[tag_name]
         if map then
@@ -183,13 +185,31 @@ M.html_to_md = function(html)
       end
     end
 
+    result = result:gsub("\n\n\n+", "\n\n")
+
     return result
   end
 
   ---@param node TSNode
-  function transpiler:eval_row(node)
-    -- TODO
+  function transpiler:eval_table(node)
+    local children = node:named_children()
     local result = ""
+    local max_col_len = {}
+
+    for i = 2, #children - 1 do
+      local child_node = children[i]
+      local child_tag_node = child_node:named_child():named_child()
+      local child_tag_name = self:get_node_text(child_tag_node)
+
+      if child_tag_name == "thead" or child_tag_name == "tbody" then
+        result = result .. self:eval_table(child_node)
+      end
+
+      if child_tag_name == "tr" then
+        local col_nodes = child_node:named_children()
+        -- TODO
+      end
+    end
 
     return result
   end
@@ -197,17 +217,32 @@ M.html_to_md = function(html)
   return transpiler:transpile()
 end
 
--- M.html_to_md([[
+-- local md = M.html_to_md([[
 -- <table>
---   <tr>
---     <td>col1</td>
---     <td>col2</td>
---   </tr>
---   <tr>2</tr>
---   <tr>3</tr>
---   <tr>4</tr>
---   <tr>5</tr>
+--   <thead>
+--     <tr>
+--       <th>Header 1</th>
+--       <th>Header 2</th>
+--     </tr>
+--   </thead>
+--   <tbody>
+--     <tr>
+--       <td>Row 1, Cell 1</td>
+--       <td>Row 1, Cell 2</td>
+--     </tr>
+--     <tr>
+--       <td>Row 2, Cell 1</td>
+--       <td>Row 2, Cell 2</td>
+--     </tr>
+--   </tbody>
 -- </table>
 -- ]])
+--
+-- print(md)
+--
+-- | Header 1     | Header 2     |
+-- | ------------ | ------------ |
+-- | Row 1, Cell 1| Row 1, Cell 2|
+-- | Row 2, Cell 1| Row 2, Cell 2|
 
 return M
